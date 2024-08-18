093658cb142f4472d4380c223d4c53e10416cd92
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-08-18 23:47:01 +0530

    Time: 37 ms (48.79%), Space: 16.5 MB (66.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,15 +3,8 @@ class Solution:
         if len(nums)==1:
             return nums[0]
         tbl = [-1] * (len(nums) + 1)
-        def calc(i):
-            if tbl[i] != -1:
-                return tbl[i]
-            if i == len(nums):
-                return 0
-            if i == len(nums) - 1:
-                return nums[i]
-            x = nums[i] + calc(i+2)
-            y = calc(i+1)
-            tbl[i] = max(x,y)
-            return tbl[i]
-        return calc(0)
+        tbl[-1] = 0
+        tbl[-2] = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            tbl[i] = max(nums[i] + tbl[i+2], tbl[i+1])
+        return max(tbl[0], tbl[1])
