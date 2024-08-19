f0deca7b3f6e276bd11dd768bc799cc555b59897
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-08-19 23:28:39 +0530

    Time: 347 ms (22.09%), Space: 16.5 MB (61.96%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        def solve(ac, prev):
+            if ac==n:
+                return 0
+            if ac>n:
+                return 1000
+            if prev == 0:
+                return 2 + solve(ac + ac, ac)
+            x=1+solve(ac+prev,prev)
+            y=2+solve(ac+ac,ac)
+            return min(x,y)
+        return solve(1, 0)
