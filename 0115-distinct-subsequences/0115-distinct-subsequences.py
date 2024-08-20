769ddfa44806a426f327b93d548dfa590c893bdc
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-08-21 00:29:23 +0530

    Time: 533 ms (22.23%), Space: 171.6 MB (10.12%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,12 +1,14 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n, m = len(s), len(t)
-        @cache
+        tbl = {}
         def comp(i, j):
+            if (i, j) in tbl: return tbl[(i, j)]
             if i == n and j == m: return 1
             if i == n : return 0
             x = comp(i + 1, j)
             if j < m and s[i] == t[j]:
                 x += comp(i + 1, j + 1)
+            tbl[(i, j)] = x
             return x
         return comp(0,0)
