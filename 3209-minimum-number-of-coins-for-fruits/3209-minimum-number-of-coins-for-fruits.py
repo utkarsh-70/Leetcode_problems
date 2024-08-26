0f6775b78db0e392d1f4a04226c5e0d4ad673132
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-08-26 16:15:56 +0530

    Time: 2344 ms (5.24%), Space: 424.7 MB (10.48%) - LeetHub

--- 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
+++ 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
@@ -1,14 +1,17 @@
 class Solution:
     def minimumCoins(self, prices: List[int]) -> int:
-        @cache
+        di={}
         def topd(i,skips):
-            if i>=len(prices):
+            if (i,skips) in di:
+                return di[(i,skips)]
+            if i==len(prices):
                 return 0
             x,y=float('inf'),float('inf')
             x=prices[i]+topd(i+1,2*i+1)
             if i<=skips:
                 y=topd(i+1,skips)
 
-            return min(x,y)
+            di[(i,skips)]=min(x,y)
+            return di[(i,skips)]
         return prices[0]+topd(1,1)
     
