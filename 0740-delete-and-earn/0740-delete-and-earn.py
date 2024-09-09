7294656cf9a84eb4753a8be2bc30e2974cfb4d7a
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-09-09 09:24:01 +0530

    Time: 91 ms (10.01%), Space: 21.5 MB (19.73%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,40 +1,24 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        unique = sorted(set(nums))
-        has = {}
-        for i in nums:
-            if i not in has:
-                has[i] = 0
-            has[i] += 1
-
-        @cache
-        def earn(i):
-            if i > unique[-1]:
-                return 0
-            if i not in has:
-                has[i] = 0
-            return max(earn(i + 1), has[i] * i + earn(i + 2) )
-
-        return earn(1)
-
-class Solution1:
-    def deleteAndEarn(self, nums: List[int]) -> int:
         di={}
-        def topd(i,s1):
-            if (i,s1) in di:
-                return di[(i,s1)]
-            if i==len(nums):
+        for i in nums:
+            if i  in di:
+                di[i]+=1
+            else:
+                di[i]=1
+        temp=list(di.keys())
+        temp.sort()
+        memo={}
+        def topd(i,prev,taken):     
+            if (i,taken) in memo:
+                return memo[(i,taken)]    
+            if i==len(temp):
                 return 0
-            x=0
-            for j in range(i,len(nums)):
-                if nums[j] not in s1:
-                    s1.append(nums[j]+1)
-                    s1.append(nums[j]-1)
-                    y=nums[j]+topd(j+1,s1)
-                    s1.remove(nums[j]+1)
-                    s1.remove(nums[j]-1)
-                    x=max(x,y)
-            di[(i,s1)]=x 
-            return di[(i,s1)]
-        return topd(0,())
-                
+            take,skip=0,0
+            if temp[i]!=prev+1:       
+                take=(di[temp[i]]*temp[i])+topd(i+1,temp[i],True)
+           
+            skip=topd(i+1,prev,False)
+            memo[(i,taken)]=max(take,skip)
+            return memo[(i,taken)]
+        return topd(0,-1,False)
