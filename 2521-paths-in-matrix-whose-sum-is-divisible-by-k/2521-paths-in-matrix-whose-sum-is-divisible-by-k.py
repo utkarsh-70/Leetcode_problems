747b90cac9a05fa895f4f29dd78ff2cbcee8c3c3
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-09-21 18:53:15 +0530

    Time: 1511 ms (95.93%), Space: 97.1 MB (54.44%) - LeetHub

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/2521-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/2521-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -1,15 +1,21 @@
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
-        mod = 10 ** 9 + 7
         m, n = len(grid), len(grid[0])
-        @cache
-        def dp(i, j, curr):
-            if i == m - 1 and j == n - 1: return int(curr == 0)
-            res = 0
-            for di, dj in [(0, 1), (1, 0)]:
-                ni, nj = i + di, j + dj
-                if ni < 0 or ni == m or nj < 0 or nj == n: continue
-                res = (res + dp(ni, nj, (curr + grid[ni][nj]) % k)) % mod
-            return res
-
-        return dp(0, 0, grid[0][0] % k) % mod
+        MOD = 10**9 + 7
+        
+        dp = [[[0] * k for _ in range(n)] for _ in range(m)]
+        dp[0][0][grid[0][0] % k] = 1
+        
+        for i in range(1, m):
+            for x in range(k):
+                dp[i][0][(x + grid[i][0]) % k] = dp[i-1][0][x]
+        for j in range(1, n):
+            for x in range(k):
+                dp[0][j][(x + grid[0][j]) % k] = dp[0][j-1][x]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                for x in range(k):
+                    dp[i][j][(x + grid[i][j]) % k] = (dp[i-1][j][x] + dp[i][j-1][x]) % MOD
+        
+        return dp[m-1][n-1][0]
