be167aa9e339f36e153cb143eb1e622d1c76d290
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-09-23 14:52:42 +0530

    Time: 1998 ms (5%), Space: 16.7 MB (15.99%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,49 @@
+class Solution1:
+    def nthUglyNumber(self, n: int) -> int:
+        temp=[2,3,5]
+        def bottomUp(i,t1,out):
+            if t1>=n:
+                print(out)
+                return out
+            x=[]
+            for j in range(i,len(temp)):
+                out.append(temp[j]*t1)
+                print(out)
+                x=bottomUp(j+1,temp[j]*t1,out)
+            return x
+        
+        out=bottomUp(0,1,[])
+        print(out)
+        return 0
+
+
+class Solution2:
+    def nthUglyNumber(self, n: int) -> int:
+        temp=[2,3,5]
+        def topD(i,t1):
+            if t1>=n:
+                return []
+            for j in range(i,len(temp)):
+                return [t1*temp[j]]+topD(j+1,t1*temp[j])
+        out=topD(0,1)
+        print(out)
+        return 0
+        
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        out = [1]  
+        temp = [2, 3, 5]  
+        idx = 0
+        x = 1  
+    
+        while x<n: 
+            val = out[idx]
+            for i in range(3):
+                new_val = val * temp[i]
+                if new_val not in out: 
+                    out.append(new_val)
+            out.sort()  
+            idx += 1
+            x += 1
+        
+        return out[n-1] 
