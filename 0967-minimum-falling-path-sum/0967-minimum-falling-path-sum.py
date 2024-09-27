93075b2f906eb53abb51978cb5ee5935471dcda4
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-09-27 09:32:54 +0530

    Time: 167 ms (13.97%), Space: 26.5 MB (11.43%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -2,8 +2,10 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         row=len(matrix)
         col=len(matrix[0])
-        @cache
+        memo={}
         def topd(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
             if i==row-1 and j>=0 and j<col:
                 return matrix[i][j]
             if j<0 or i>=row or j>=col:
@@ -11,7 +13,8 @@ class Solution:
             bott=matrix[i][j]+topd(i+1,j)
             rdiag=matrix[i][j]+topd(i+1,j+1)
             ldiag=matrix[i][j]+topd(i+1,j-1)
-            return min(bott,rdiag,ldiag)
+            memo[(i,j)]= min(bott,rdiag,ldiag)
+            return memo[(i,j)]
         min1=float('inf')
         for j in range(col):
             min1=min(min1,topd(0,j))
