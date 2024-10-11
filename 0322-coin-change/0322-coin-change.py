479cfef4797267cf25196e932a14b2824833d99f
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-10-11 16:16:13 +0530

    Time: 1321 ms (9.55%), Space: 50.5 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,7 +2,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
         di={}
-        # coins.sort()
+        coins.sort()
         def topd(amount):
             if amount==0:
                 return 0
@@ -15,6 +15,8 @@ class Solution:
             for j in range(0,n):
                 if amount-coins[j]>=0:
                     min_val=min(min_val,1+topd(amount-coins[j]))
+                else:
+                    break
             di[amount]=min_val
             return di[amount]
         x=topd(amount)
