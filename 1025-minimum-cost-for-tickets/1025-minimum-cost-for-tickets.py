6090bf001aaaa7120af87b1163014a16369e16cb
utkarsh-70 <120498876+utkarsh-70@users.noreply.github.com>
2024-10-11 20:36:06 +0530

    Time: 66 ms (7.73%), Space: 19.1 MB (5.37%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        days.sort()
+        @cache
+        def topd(i,prev):
+            if i==len(days):
+                return 0
+            if days[i]<=prev:
+                return topd(i+1,prev)
+            else:
+                c1,c2,c3=float('inf'),float('inf'),float('inf')
+                c1=costs[0]+topd(i+1,days[i])
+                c2=costs[1]+topd(i+1,days[i]+6)
+                c3=costs[2]+topd(i+1,days[i]+29)
+                return min(c1,c2,c3)
+                
+        return topd(0,0)
